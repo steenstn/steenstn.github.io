170f6398438f920e1348dd1db611fd31ce2b57f5
steenstn <alest849@gmail.com>
2017-02-10 18:17:52 +0100

    Refactored flying strategy

--- game/new/flyingStrategy.js
+++ game/new/flyingStrategy.js
@@ -1,42 +1,42 @@
 var flyingStrategy = function(tilesize, levelWidth, currentLevel) {
 	this.move = function(enemy) {
+		var xOffset = 0;
+		var yOffset = 15;
 		if(enemy.speedx>0) // Going right
-			{
-				arrayPos=Math.floor((enemy.x+20)/tilesize)+Math.floor((enemy.y+15)/tilesize)*levelWidth; // The position in the level array(Middle of the enemy at the moment)
-				if(currentLevel[arrayPos].blocking==1 || currentLevel[arrayPos].type=="h" )
-				{
-					enemy.speedx*=-1;
-				}
-				
-			}
-			else if(enemy.speedx<0) // Going left
-			{
-				arrayPos=Math.floor((enemy.x)/tilesize)+Math.floor((enemy.y+15)/tilesize)*levelWidth; // The position in the level array(Middle of the enemy at the moment)
-				if(currentLevel[arrayPos].blocking==1 || currentLevel[arrayPos].type=="h" )
-				{
-					enemy.speedx*=-1;
-				}
-				
-			}
-			if(enemy.speedy<0) // Going up up up
-			{
-				arrayPos=Math.floor((enemy.x+10)/tilesize)+Math.floor((enemy.y-1)/tilesize)*levelWidth; // The position in the level array(Middle of the enemy at the moment)
-				if(currentLevel[arrayPos].blocking==1 || currentLevel[arrayPos].type=="h" )
-				{
-					enemy.speedy*=-1;
-				}
-				
-			}
-			else if(enemy.speedy>0) // Going down
-			{
-				arrayPos=Math.floor((enemy.x+10)/tilesize)+Math.floor((enemy.y+31)/tilesize)*levelWidth; // The position in the level array(Middle of the enemy at the moment)
-				if(currentLevel[arrayPos].blocking==1 || currentLevel[arrayPos].type=="h" )
-				{
-					enemy.speedy*=-1;
-				}
-				
-			}
-			enemy.x+=enemy.speedx;
-			enemy.y+=enemy.speedy;
-			}
+		{
+			xOffset = 20;
+		}
+		else if(enemy.speedx<0) // Going left
+		{
+			xOffset = 0;
+		}
+
+		arrayPos=Math.floor((enemy.x+xOffset)/tilesize)+Math.floor((enemy.y+yOffset)/tilesize)*levelWidth; // The position in the level array(Middle of the enemy at the moment)
+
+		if(currentLevel[arrayPos].blocking==1 || currentLevel[arrayPos].type=="h" )
+		{
+			enemy.speedx*=-1;
+		}
+
+		xOffset = 10;
+
+		if(enemy.speedy<0) // Going up up up
+		{
+			yOffset = -1;
+		}
+		else if(enemy.speedy>0) // Going down
+		{
+			yOffset = 31;
+		}
+
+		arrayPos=Math.floor((enemy.x+xOffset)/tilesize)+Math.floor((enemy.y+yOffset)/tilesize)*levelWidth; // The position in the level array(Middle of the enemy at the moment)
+
+		if(currentLevel[arrayPos].blocking==1 || currentLevel[arrayPos].type=="h" )
+		{
+			enemy.speedy*=-1;
+		}
+
+		enemy.x+=enemy.speedx;
+		enemy.y+=enemy.speedy;
+		}
 }
