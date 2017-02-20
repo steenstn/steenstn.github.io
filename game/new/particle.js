711b1030e83246451d1317126975460894bcacdd
steenstn <alest849@gmail.com>
2017-02-20 20:04:31 +0100

    #2 Blood dripping

--- game/new/particle.js
+++ game/new/particle.js
@@ -1,18 +1,26 @@
 function Particle(x, y, xSpeed, ySpeed, image) {
+  var ACTIVE = 1;
+  var INACTIVE = 0;
+  var DRIPPING = 2;
   this.x = x;
   this.y = y;
   this.xSpeed = xSpeed;
   this.ySpeed = ySpeed;
-  this.active = true;
+
+  this.state = ACTIVE;
   this.xFriction = 1.1;
   this.image = image;
   this.move = function() {
 
-    if(this.active) {
+    if(this.state === ACTIVE) {
       this.x+=this.xSpeed;
       this.y+=this.ySpeed;
       this.ySpeed+=0.3;
       this.xSpeed/=this.xFriction;
+    } else if (this.state === DRIPPING) {
+      this.ySpeed = 0.2
+      this.xSpeed = 0;
+      this.y+=this.ySpeed;
     }
   }
 
