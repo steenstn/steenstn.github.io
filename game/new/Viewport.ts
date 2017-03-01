7719c7217a8eb99ecc22aedf8d19138f49693990
steenstn <alest849@gmail.com>
2017-03-01 20:27:50 +0100

    Moved viewport movement to class

--- game/new/Viewport.ts
+++ game/new/Viewport.ts
@@ -3,6 +3,10 @@ class Viewport {
   static y : number;
   static oldx : number;
   static oldy : number;
+  static width = 800;
+  static height = 450;
+  static numTilesInScreenWidth = Math.round(Viewport.width / Level.tileSize);
+  static numTilesInScreenHeight = Math.round(Viewport.height / Level.tileSize);
   constructor() {
 
     Viewport.x = 0;
@@ -10,4 +14,28 @@ class Viewport {
     Viewport.oldx = Viewport.x;
     Viewport.oldy = Viewport.y;
   }
+
+  static move(x1, y1, x2, y2) {
+    let xMidpoint = (x1 + x2) / 2;
+    let yMidpoint = (y1 + y2) / 2;
+
+    Viewport.x=Math.round(-xMidpoint+(Viewport.numTilesInScreenWidth / 2)*Level.tileSize);
+  	Viewport.y=Math.round(-yMidpoint+(Viewport.numTilesInScreenHeight / 2)*Level.tileSize);
+
+
+
+  	// Do not move the screen outside of the level
+  	if(Viewport.x>0)
+  		Viewport.x=0;
+  	if(Viewport.x<-Level.width*Level.tileSize+Viewport.numTilesInScreenWidth*Level.tileSize)
+  		Viewport.x=-Level.width*Level.tileSize+Viewport.numTilesInScreenWidth*Level.tileSize;
+
+  	if(Viewport.y>0)
+  		Viewport.y=0;
+  	if(Viewport.y<-Level.height*Level.tileSize+Viewport.numTilesInScreenHeight*Level.tileSize)
+  		Viewport.y=-Level.height*Level.tileSize+Viewport.numTilesInScreenHeight*Level.tileSize
+
+  }
+
+
 }
--- game/new/js/Viewport.js
+++ game/new/js/Viewport.js
@@ -5,5 +5,23 @@ var Viewport = (function () {
         Viewport.oldx = Viewport.x;
         Viewport.oldy = Viewport.y;
     }
+    Viewport.move = function (x1, y1, x2, y2) {
+        var xMidpoint = (x1 + x2) / 2;
+        var yMidpoint = (y1 + y2) / 2;
+        Viewport.x = Math.round(-xMidpoint + (Viewport.numTilesInScreenWidth / 2) * Level.tileSize);
+        Viewport.y = Math.round(-yMidpoint + (Viewport.numTilesInScreenHeight / 2) * Level.tileSize);
+        if (Viewport.x > 0)
+            Viewport.x = 0;
+        if (Viewport.x < -Level.width * Level.tileSize + Viewport.numTilesInScreenWidth * Level.tileSize)
+            Viewport.x = -Level.width * Level.tileSize + Viewport.numTilesInScreenWidth * Level.tileSize;
+        if (Viewport.y > 0)
+            Viewport.y = 0;
+        if (Viewport.y < -Level.height * Level.tileSize + Viewport.numTilesInScreenHeight * Level.tileSize)
+            Viewport.y = -Level.height * Level.tileSize + Viewport.numTilesInScreenHeight * Level.tileSize;
+    };
     return Viewport;
 }());
+Viewport.width = 800;
+Viewport.height = 450;
+Viewport.numTilesInScreenWidth = Math.round(Viewport.width / Level.tileSize);
+Viewport.numTilesInScreenHeight = Math.round(Viewport.height / Level.tileSize);
