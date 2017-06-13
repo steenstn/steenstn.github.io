2aae21e053235bc147aea8e37d44e4df4b103c8f
steenstn <alest849@gmail.com>
2017-06-13 18:55:11 +0200

    Refactoring, moved world constants to own class

---
+++ game/new/WorldConstants.ts
@@ -0,0 +1,12 @@
+class WorldConstants {
+  static kickbackForce : number = 7;
+  static bloodAmount : number = 10;
+  static playerAcc : number = 0.3;
+  static gravity : number = 0.3;
+  static jumpStrength : number = 7;
+  static maxSpeedx : number = 3;
+  static maxSpeedy : number = 6;
+  static jumpAcc : number = 2;
+  static maxJump : number = 7;
+  static normalFriction : number = 1.2;
+}
---
+++ game/new/js/WorldConstants.js
@@ -0,0 +1,15 @@
+var WorldConstants = (function () {
+    function WorldConstants() {
+    }
+    return WorldConstants;
+}());
+WorldConstants.kickbackForce = 7;
+WorldConstants.bloodAmount = 10;
+WorldConstants.playerAcc = 0.3;
+WorldConstants.gravity = 0.3;
+WorldConstants.jumpStrength = 7;
+WorldConstants.maxSpeedx = 3;
+WorldConstants.maxSpeedy = 6;
+WorldConstants.jumpAcc = 2;
+WorldConstants.maxJump = 7;
+WorldConstants.normalFriction = 1.2;
