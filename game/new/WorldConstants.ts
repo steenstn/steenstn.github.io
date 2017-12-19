878b6eb5c3f12458d354803bad5b4a146d43712c
steenstn <alest849@gmail.com>
2017-12-19 09:39:21 +0100

    Inreased running speed and changel first level

--- game/new/WorldConstants.ts
+++ game/new/WorldConstants.ts
@@ -4,7 +4,7 @@ class WorldConstants {
   static playerAcc : number = 0.3;
   static gravity : number = 0.3;
   static jumpStrength : number = 7;
-  static maxSpeedx : number = 3;
+  static maxSpeedx : number = 3.2;
   static maxSpeedy : number = 6;
   static jumpAcc : number = 2;
   static maxJump : number = 7;
