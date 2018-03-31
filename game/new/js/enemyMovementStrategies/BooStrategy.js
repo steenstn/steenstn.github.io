3a80251b5609e559a933cbb8919943f76118e39a
Alexander Steen <alest849@gmail.com>
2018-03-31 19:00:14 +0200

    Removed unused class

--- game/new/js/enemyMovementStrategies/BooStrategy.js
+++
@@ -1,9 +0,0 @@
-var BooStrategy = (function () {
-    function BooStrategy(enemy, players) {
-        this.enemy = enemy;
-        this.players = players;
-    }
-    BooStrategy.prototype.move = function () {
-    };
-    return BooStrategy;
-}());
